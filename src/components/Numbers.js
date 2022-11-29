15fbb589bbf05494cca752d24219caf96d894953
Prudhvi Raj <prudhvi@123.com>
2022-11-29 22:35:49 +0530

    count completed

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,22 +1,32 @@
 import React from "react";
+import styles from "../styles/Numbers.module.css";
+import location from "../Assets/location.svg";
+import user from "../Assets/user.svg";
+import server from "../Assets/Server.svg";
 
 function Numbers() {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-        <h3>90+</h3>
-        <p>users</p>
+    <div className={styles.container}>
+      <div className={styles.count}>
+        <img src={user} alt="user" />
+        <div>
+          <h3>90+</h3>
+          <p>Users</p>
+        </div>
       </div>
-      <div>
-        <img src="" alt="" />
-        <h3>90+</h3>
-        <p>locations</p>
+      <div className={styles.count}>
+        <img src={location} alt="location" />
+        <div>
+          <h3>30+</h3>
+          <p>Locations</p>
+        </div>
       </div>
-      <div>
-        <img src="" alt="" />
-        <h3>90+</h3>
-        <p>servers</p>
+      <div className={styles.count}>
+        <img src={server} alt="server" />
+        <div>
+          <h3>50+</h3>
+          <p>Servers</p>
+        </div>
       </div>
     </div>
   );
