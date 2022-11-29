940c5f5aa4f913fe398d43fb911a4d127a2d9783
Prudhvi Raj <prudhvi@123.com>
2022-11-29 22:09:30 +0530

    summary completed

--- src/components/Summery.js
+++ src/components/Summery.js
@@ -1,15 +1,19 @@
 import React from "react";
+import styles from "../styles/Summary.module.css";
+import ill1 from "../Assets/Illustration 1.svg";
 
 function Summery() {
   return (
-    <div>
-      <h2>Want anything to be easy with LaslesVPN.</h2>
-      <p>
-        Provide a network for all your needs with ease and fun using LaslesVPN
-        discover interesting features from us.
-      </p>
-      <img src="" alt="" />
-      <button>Get Started</button>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1>Want anything to be easy with LaslesVPN.</h1>
+        <p>
+          Provide a network for all your needs with ease and fun using LaslesVPN
+          discover interesting features from us.
+        </p>
+        <button>Get Started</button>
+      </div>
+      <img src={ill1} alt="ill1" />
     </div>
   );
 }
