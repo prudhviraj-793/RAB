664b12b6444bfc831cba8ed95c3619f23defe7db
Prudhvi Raj <prudhvi@123.com>
2022-11-29 22:58:30 +0530

    services completed

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,20 +1,24 @@
 import React from "react";
+import ill2 from "../Assets/Illustration 2.svg";
+import styles from "../styles/Services.module.css";
 
 function Services() {
   return (
-    <div>
-      <img src="" alt="" />
-      <h3>We Provide Many Features You Can Use</h3>
-      <p>
-        You can explore the features that we provide with fun and have their own
-        functions each feature.
-      </p>
-      <ul>
-        <li>Powerfull online protection.</li>
-        <li>Powerfull online protection.</li>
-        <li>Powerfull online protection.</li>
-        <li>Powerfull online protection.</li>
-      </ul>
+    <div className={styles.container}>
+      <img src={ill2} alt="ill2" />
+      <div className={styles.services}>
+        <h2>We Provide Many Features You Can Use</h2>
+        <p>
+          You can explore the features that we provide with fun and have their
+          own functions each feature.
+        </p>
+        <ul>
+          <li>Powerfull online protection.</li>
+          <li>Powerfull online protection.</li>
+          <li>Powerfull online protection.</li>
+          <li>Powerfull online protection.</li>
+        </ul>
+      </div>
     </div>
   );
 }
