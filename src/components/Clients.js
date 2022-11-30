036a7f326cd2b00b8ac1d91cebde83d8e1b1f160
Prudhvi Raj <prudhvi@123.com>
2022-11-30 07:14:29 +0530

    clients completed

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,13 +1,19 @@
 import React from "react";
+import client1 from "../Assets/Mask Group.svg";
+import client2 from "../Assets/Mask Group (1).svg";
+import client3 from "../Assets/Mask Group (2).svg";
+import client4 from "../Assets/Mask Group (3).svg";
+import client5 from "../Assets/Mask Group (4).svg";
+import styles from '../styles/Clients.module.css'
 
 function Clients() {
   return (
-    <div>
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
+    <div className={styles.container}>
+      <img src={client1} alt="client1" />
+      <img src={client2} alt="client2" />
+      <img src={client3} alt="client3" />
+      <img src={client4} alt="client4" />
+      <img src={client5} alt="client5" />
     </div>
   );
 }
