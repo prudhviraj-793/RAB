289b6079df0542ba85bafeefcc7067e530657a80
Prudhvi Raj <prudhvi@123.com>
2022-11-30 07:07:02 +0530

    network completed

--- src/components/Network.js
+++ src/components/Network.js
@@ -1,16 +1,18 @@
 import React from "react";
+import global from '../Assets/Huge Global.svg'
+import styles from '../styles/Network.module.css'
 
 function Network() {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h3>Huge Global Network of Fast VPN</h3>
         <p>
           See LaslesVPN everywhere to make it easier for you when you move
           locations.
         </p>
       </div>
-      <img src="" alt="" />
+      <img src={global} alt="global" />
     </div>
   );
 }
