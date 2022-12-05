35d372e7b60c8ed7c40222aca44ff97fbc886fa4
Prudhvi Raj <prudhvi@123.com>
2022-12-05 16:31:18 +0530

    subscriber completed

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,13 +1,14 @@
 import React from "react";
+import styles from '../styles/Subscribe.module.css'
 
 function Subscribe() {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h3>Subscribe Now for Get Special Features!</h3>
         <p>Let's subscribe with us and find the fun.</p>
       </div>
-      <div>
+      <div className={styles.subBtn}>
         <button>Subscribe Now</button>
       </div>
     </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,24 +1,82 @@
 import React from "react";
+import styles from "../styles/Testimonials.module.css";
+import user1 from "../Assets/Ellipse 175.png";
+import user2 from "../Assets/Ellipse 175 (1).png";
+import user3 from "../Assets/Ellipse 175 (2).png";
 
 function Testimonials() {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h3>Trusted by Thousands of Happy Customer</h3>
         <p>
           These are the stories of our customers who have joined us with great
           pleasure when using this crazy feature.
         </p>
       </div>
-      <div>
-        <img src="" alt="" />
-        <h5>Viezh Robert</h5>
-        <p>Warsaw, Poland</p>
-        <p>
-          “Wow... I am very happy to use this VPN, it turned out to be more than
-          my expectations and so far there have been no problems. LaslesVPN
-          always the best”.
-        </p>
+      <div className={styles.users}>
+        <div className={styles.user}>
+          <div className={styles.userProfile}>
+            <img src={user1} alt="user1" />
+            <div className={styles.userName}>
+              <h5>Viezh Robert</h5>
+              <p>Warsaw, Poland</p>
+            </div>
+          </div>
+          <div className={styles.review}>
+            <p>
+              “Wow... I am very happy to use this VPN, it turned out to be more
+              than my expectations and so far there have been no problems.
+              LaslesVPN always the best”.
+            </p>
+          </div>
+        </div>
+        <div className={styles.user}>
+          <div className={styles.userProfile}>
+            <img src={user2} alt="user2" />
+            <div className={styles.userName}>
+              <h5>Yessica Christy</h5>
+              <p>Shanxi, China</p>
+            </div>
+          </div>
+          <div className={styles.review}>
+            <p>
+              “I like it because I like to travel far and still can connect with
+              high speed.”.
+            </p>
+          </div>
+        </div>
+        <div className={styles.user}>
+          <div className={styles.userProfile}>
+            <img src={user3} alt="user3" />
+            <div className={styles.userName}>
+              <h5>Kim Young Jou</h5>
+              <p>Seoul, South Korea</p>
+            </div>
+          </div>
+          <div className={styles.review}>
+            <p>
+              “This is very unusual for my business that currently requires a
+              virtual private network that has high security.”.
+            </p>
+          </div>
+        </div>
+        <div className={styles.user}>
+          <div className={styles.userProfile}>
+            <img src={user1} alt="user1" />
+            <div className={styles.userName}>
+              <h5>Viezh Robert</h5>
+              <p>Warsaw, Poland</p>
+            </div>
+          </div>
+          <div className={styles.review}>
+            <p>
+              “Wow... I am very happy to use this VPN, it turned out to be more
+              than my expectations and so far there have been no problems.
+              LaslesVPN always the best”.
+            </p>
+          </div>
+        </div>
       </div>
       <div>
         <button>=</button>
